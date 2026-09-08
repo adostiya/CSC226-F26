0dd0f5d4850e4458f723153d6d1365238371120f
Kadyr D <adostiya@carrollu.edu>
2026-09-08 01:43:17 +0000

    Finished

--- InClassNotes/src/main/java/week1/Counter.java
+++ InClassNotes/src/main/java/week1/Counter.java
@@ -5,15 +5,19 @@ public class Counter {
     // 2. Add a private field
     // TODO: Declare private int count;
     private int count;
+    private int[] history = new int[5];
+    private int size = 0;
 
     // 3. Write the default constructor
     public Counter(){
         count = 0;
+        historyclass()
     }
 
     // 4. Add an alternate constructor
     public Counter(int count){
         this.count = count;
+        historyclass()
     }
     // 5. Write an accessor method
     public int getCount(){
@@ -21,33 +25,42 @@ public class Counter {
     }
 
     // 6. Add an update method
-    public void deacreaseCount(){
-        count = count--;
+    public void decreaseCount(){
+        count--;
+        historyclass()
     }
     
     // 7. Overload increment
     public void increaseCount(){
-        count = count++;
+        count++;
+        historyclass()
     }
     public void increaseCount(int num){
         count = count+num;
+        historyclass()
     }
     public void increaseCount(int num, int num2){
         count = count+num+num2;
+        historyclass()
     }
    
     // 8. Add a reset method
     public void resetCount(){
         count = 0;
+        historyclass()
     }
 
     //9. Add a fixed-size history array that records the last 5 states of the counter. All logic must happen inside of the object and be hidden from the user.
-    private void history(int g) {
-        int[] counters = new int[5];
-        int i = 0;
-        counters[i] = g;
-        if (i>5) {
-            counter = 
+    private void historyclass() {
+        if (size < 5) {
+            history[size] = count;
+            size++;
+        }
+        else {
+            for (int i=0; i<4; i++) {
+                history[i] = history[i+1];
+            }
+            history [4] = count;
         }
     }
 }
